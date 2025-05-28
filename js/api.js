809fb0a42abf1150dcf9d85fb1157f82e1c639c6
coffeetron832 <jpontonh@gmail.com>
2025-05-28 09:23:13 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,17 @@
+const API_URL = "https://momento-backend-production.up.railway.app"; // Cámbialo si es necesario
+
+async function apiRequest(endpoint, method, data, token = null) {
+  const config = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (token) config.headers["Authorization"] = `Bearer ${token}`;
+  if (data) config.body = JSON.stringify(data);
+
+  const res = await fetch(`${API_URL}${endpoint}`, config);
+  return res.json();
+}
 
